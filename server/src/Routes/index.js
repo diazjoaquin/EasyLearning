345068729ebc45a99d35cffd68399cc30d178317
Fran3003 <franco.rantuchoQgmail.com>
2022-12-06 19:20:23 -0300

    RouteCategories listo

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 const getAllCourses = require("./getAllCourses/getAllCourses.js");
+const getAllCategories = require("./get all categories/getAllCategories.js")
+
 
 router.use("/getAllCourses", getAllCourses);
+router.use("/categories", getAllCategories)
 
 module.exports = router;
