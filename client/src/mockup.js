289ef520d66aa36115cf47144131e0296e7e5029
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2022-12-06 13:49:32 -0300

    mockup added and more dependencies

---
+++ client/src/mockup.js
@@ -0,0 +1,37 @@
+export const courses = [
+    {
+        idCourse: 1,
+        Description: "The best course to learn how to create your first repository on GitHub.",
+        Teacher: "Franco Cartucho",
+        Students: ["German", "Bianca", "Fermin"],
+        Rating: "4.5",
+        Video: ["https://www.youtube.com/watch?v=C6IjS7jKnjQ", "https://www.youtube.com/watch?v=vlCXdvcgiE0", "https://www.youtube.com/watch?v=DinilgacaWs",]
+    },
+
+    {
+        idCourse: 2,
+        Description: "On this course you will leran how to steal your friend's girlfriend.",
+        Teacher: "Santiago Grosso",
+        Students: ["joaco", "franco", "luquitas"],
+        Rating: "5.0",
+        Video: ["https://www.youtube.com/watch?v=wXeVIchxiL0", "https://www.youtube.com/watch?v=2jxc8DMzt0I", "https://www.youtube.com/watch?v=SigVVJmUGv8",]
+    },
+
+    {
+        idCourse: 3,
+        Description: "The best course on the market. You will learn how to produce lofi music from beggining to end.",
+        Teacher: "Tu morenitoh",
+        Students: ["luquitas", "franco", "benja gangolo"],
+        Rating: "2.0",
+        Video: ["https://www.youtube.com/watch?v=vkQcpY3T8T4", "https://www.youtube.com/watch?v=p0CNK3eV1yA", "https://www.youtube.com/watch?v=GvcGUF7qznQ",]
+    },
+
+    {
+        idCourse: 4,
+        Description: "On this course you will learn how to use Amazon Web Services.",
+        Teacher: "Bianca",
+        Students: ["Joaco", "Franco Cartucho", "benja gangolo"],
+        Rating: "1.5",
+        Video: ["https://www.youtube.com/watch?v=SOTamWNgDKc", "https://www.youtube.com/watch?v=Z0EtjKNRdR4", "https://www.youtube.com/watch?v=g2JOHLHh4rI",]
+    },
+]
