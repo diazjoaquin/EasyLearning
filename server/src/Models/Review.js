ccf537583210ffed92df2d78c7655d3f4c077698
SantiRosso <santiagorosso@live.com.ar>
2022-12-06 14:58:29 -0300

    Modelos/Santi terminados

---
+++ server/src/Models/Review.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "review",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      comments: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
