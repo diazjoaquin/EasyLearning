121fc5916f0691b501af48bb9c73b182296e1954
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2022-12-07 15:58:11 -0300

    redux-DevTools, Provider and Store added

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,6 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootRouter from "../reducer";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducer";
 import thunk from "redux-thunk";
 
-export const store = createStore(rootRouter, composeWithDevTools(applyMiddleware(thunk)));
+const composeEnhancers =
+   (typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+   compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk)),
+ );
+
+ export default store;
