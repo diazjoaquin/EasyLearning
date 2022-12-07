f65e7c70c5b94300c9accb885767070272764c75
ggolder22 <germanxvzy985@hotmail.com>
2022-12-07 15:56:24 -0300

    search bar modified

--- client/src/components/searchbar/SearchBar.jsx
+++ client/src/components/searchbar/SearchBar.jsx
@@ -1,51 +1,50 @@
 import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import {getCourseByName, loadingAction} from "../../redux/actions"
-//import Lupa from "../../image/Lupa.png"
+import {getCourseByName, loadingAction, byName} from "../../redux/actions"
+import Lupa from "../../image/lupa.png"
 import searchbar from "../../image/searchbar.png"
 
 
 
 export default function SearchBar() {
 
-    // const dispatch = useDispatch();
-    // const [name, setName] = useState("");
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
 
 
-    // const handleInputChange = (e) => {
-    //     setName(e.target.value);
-    // };
+    const handleInputChange = (e) => {
+        setName(e.target.value);
+    };
 
-    // const handleSubmit = () =>  {
-    //     if (name) {
-    //         dispatch(byName(name));
-    //         setName("");
+    const handleSubmit = () =>  {
+        if (name) {
+            dispatch(byName(name));
+            setName("");
     //         dispatch(loadingAction(true));
     //         setTimeout(() => {
     //             dispatch(loadingAction(false));
     //         }, 3000);
-    //     } else {
-    //         alert("Debes escribir algo...");
-    //     }
-    // };
+        } else {
+            alert("Debes escribir algo...");
+        }
+    };
 
 
 
     return (
-        <div>
+        // <div>
             
-            <img src={searchbar} alt="SearchBar"/>
+        //     <img src={searchbar} alt="SearchBar"/>
+
+        // </div>
+        <div>
+            <input type="text" placeholder="Search..." onChange={handleInputChange} value={name} />
+            <div>
+                <img src={Lupa} alt="Search" onClick={(e) => handleSubmit(e)} />
+            </div>
+
 
         </div>
-    //     <div>
-    //         <input type="text" placeholder="Search..." onChange={handleInputChange} value={name} />
-    //         <div>
-    //             <img src={Lupa} alt="Search" onClick={(e) => handleSubmit(e)} />
-    //         </div>
-
-
-    //     </div>
-    // )
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <ChakraProvider>
+    
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </ChakraProvider>
+    
   </BrowserRouter>
 );
 
