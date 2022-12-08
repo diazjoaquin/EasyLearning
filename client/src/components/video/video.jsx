d1463dd708498fe858bdeb0f0366a269d2828356
SantiRosso <santiagorosso@live.com.ar>
2022-12-08 17:54:32 -0300

    new components: filters, video, comments

--- client/src/components/video/video.jsx
+++ client/src/components/video/video.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Review from "../../../../server/src/Models/Review";
+import Reviews from "../reviews/reviews";
 
-const Video = () => {
-
+const Video = (/* {id, video, reviews} */) => {
+    return(
+        <div>
+            {/* {
+                video?.map(e => {
+                    return(
+                        <p>{e}</p>
+                    )
+                })
+            }
+            
+            <Reviews reviews={reviews} id={id}/> */}
+        </div>
+    )
 }
 
 export default Video;
