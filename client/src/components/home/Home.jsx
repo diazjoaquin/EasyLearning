6debc824365792aa4004ddca8615730dc66ba8e8
ggolder22 <germanxvzy985@hotmail.com>
2022-12-09 17:43:35 -0300

    Footer added with 2 posibilities

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -7,6 +7,21 @@ import { courses } from "../../mockup";
 import { Heading, Box, Text, Image } from '@chakra-ui/react'
 import mainpicture from "../../image/maintextimage.png"
 import { Icon, ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
+import {
+
+    Container,
+    Link,
+    SimpleGrid,
+    Stack,
+    Flex,
+    Tag,
+    useColorModeValue,
+    
+} from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import Footer from "../footer/Footer";
+import Footer2 from "../footer/Footer2";
+
 // import { useSelector } from "react-redux";
 
 
@@ -14,8 +29,8 @@ export default function Home() {
 
     // carrousel: 
     // const courses = useSelector(state => state.courses);
-    const [ coursesPerPage ] = useState(3);
-    const [ currentPage, setCurrentPage ] = useState(1);
+    const [coursesPerPage] = useState(3);
+    const [currentPage, setCurrentPage] = useState(1);
     const last = currentPage * coursesPerPage;
     const first = last - coursesPerPage;
     const currentCourses = courses.slice(first, last);
@@ -30,6 +45,14 @@ export default function Home() {
         e.preventDefault();
         currentPage > 1 ? setCurrentPage(currentPage - 1) : setCurrentPage(numOfPages);
     }
+
+    const ListHeader = ({ children }) => {
+        return (
+          <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+            {children}
+          </Text>
+        );
+      };
 
     return (
         //navbar
@@ -67,8 +90,8 @@ export default function Home() {
 
                 </Carousel> */}
                 <div className={style.coursecont}>
-                <Icon as={ArrowLeftIcon} onClick={(e) => handlePrevious(e)}
-                className={style.icon}/>
+                    <Icon as={ArrowLeftIcon} onClick={(e) => handlePrevious(e)}
+                        className={style.icon} />
                     {
                         currentCourses.map((course) => {
                             return (
@@ -82,10 +105,93 @@ export default function Home() {
                         })
                     }
                     <Icon as={ArrowRightIcon} onClick={(e) => handleNext(e)}
-                    className={style.icon}/>
+                        className={style.icon} />
                 </div>
             </div>
             <br />
+            {/* <div>
+                <Box
+                    bg={useColorModeValue('gray.50', 'gray.900')}
+                    color={useColorModeValue('gray.700', 'gray.200')}>
+                    <Container as={Stack} maxW={'6xl'} py={10}>
+                        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+                            <Stack align={'flex-start'}>
+                                <ListHeader>Product</ListHeader>
+                                <Link href={'#'}>Home</Link>
+                                <Stack direction={'row'} align={'center'} spacing={2}>
+                                    <Link href={'#'}>Features</Link>
+                                    <Tag
+                                        size={'sm'}
+                                        bg={useColorModeValue('green.300', 'green.800')}
+                                        ml={2}
+                                        color={'white'}>
+                                        New
+                                    </Tag>
+                                </Stack>
+                                <Link href={'#'}>Course</Link>
+                                <Link href={'#'}>Blog</Link>
+                                <Link href={'#'}>Releases</Link>
+                            </Stack>
+                            <Stack align={'flex-start'}>
+                                <ListHeader>Company</ListHeader>
+                                <Link href={'#'}>About Us</Link>
+                                <Link href={'#'}>Press</Link>
+                                <Link href={'#'}>Careers</Link>
+                                <Link href={'#'}>Contact Us</Link>
+                                <Link href={'#'}>Partners</Link>
+                            </Stack>
+                            <Stack align={'flex-start'}>
+                                <ListHeader>Legal</ListHeader>
+                                <Link href={'#'}>Cookies Policy</Link>
+                                <Link href={'#'}>Privacy Policy</Link>
+                                <Link href={'#'}>Terms of Service</Link>
+                                <Link href={'#'}>Law Enforcement</Link>
+                                <Link href={'#'}>Status</Link>
+                            </Stack>
+                            <Stack align={'flex-start'}>
+                                <ListHeader>Follow Us</ListHeader>
+                                <Link href={'#'}>Facebook</Link>
+                                <Link href={'#'}>Twitter</Link>
+                                <Link href={'#'}>Dribbble</Link>
+                                <Link href={'#'}>Instagram</Link>
+                                <Link href={'#'}>LinkedIn</Link>
+                            </Stack>
+                        </SimpleGrid>
+                    </Container>
+                    <Box py={10}>
+                        <Flex
+                            align={'center'}
+                            _before={{
+                                content: '""',
+                                borderBottom: '1px solid',
+                                borderColor: useColorModeValue('gray.200', 'gray.700'),
+                                flexGrow: 1,
+                                mr: 8,
+                            }}
+                            _after={{
+                                content: '""',
+                                borderBottom: '1px solid',
+                                borderColor: useColorModeValue('gray.200', 'gray.700'),
+                                flexGrow: 1,
+                                ml: 8,
+                            }}>
+                            
+                        </Flex>
+                        <Text pt={6} fontSize={'sm'} textAlign={'center'}>
+                            © 2022 easyLeraning. All rights reserved
+                        </Text>
+                    </Box>
+                </Box>
+
+            </div> */}
+
+            <div>
+                <Footer2>
+
+                </Footer2>
+            </div>
+
+
         </div>
         //Texto pincipal 
     );
