26e3c79d52e42be50a3b9f99f9ff93241571be91
fermin234 <fermin234@hotmail.com>
2022-12-12 11:27:51 -0300

    refactor: client/src/comments.jsx

--- client/src/components/Carpeta videos/comments/Comments.jsx
+++ client/src/components/Carpeta videos/comments/Comments.jsx
@@ -1,56 +1,59 @@
 import { useState } from "react";
 import axios from "axios";
 
-const Comments = ({comments, videoId}) => {
+const Comments = ({ comments, videoId }) => {
 
-const [comment, setComment] = useState({
+  const [comment, setComment] = useState({
     videoId,
-    userId: 13,
+    userId: null,
     title: "",
     description: "",
-})
+  })
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     setComment({
-            ...comment,
-            [e.target.name]: e.target.value
-        });
-}
+      ...comment,
+      [e.target.name]: e.target.value
+    });
+  }
 
-const handleSubmit = async (e) => {
-    e.preventDefault() //hay que cambiar esto por un estado local, para que se actualice solamente los comentarios cada vez que alguien comenta.
-    await axios.post("http://localhost:3001/createCommentVideo",comment);
-}
+  const handleSubmit = async (e) => {
+    //e.preventDefault() //hay que cambiar esto por un estado local, para que se actualice solamente los comentarios cada vez que alguien comenta.
+    await axios.post("http://localhost:3001/createCommentVideo", comment);
+  }
 
-return(
+  return (
     <div>
-        <h2>Comentarios: </h2>
-        {/* form create comments */}
-        <form  onSubmit={handleSubmit} >
+      <h2>Comentarios: </h2>
+      {/* form create comments */}
+      <form onSubmit={handleSubmit} >
         <label>Crear comentarios</label>
-        <br/>
+        <br />
+        <label>userId: </label>
+        <input type="number" name="userId" onChange={handleChange} />
+        <br />
         <label>Title: </label>
         <input type="text" name="title" onChange={handleChange} />
-        <br/>
+        <br />
         <label>Description: </label>
         <input type="text" name="description" onChange={handleChange} />
-        <br/>
+        <br />
         <button type="submit">Comentar</button>
-        </form>
-        {
-            comments?.map((el, i) => {
-                return (                   
-                        <div key={i}>
-                            {/* <p>nombre del user</p> */}
-                            <p>{el.title}</p>
-                            <p>{el.description}</p>
-                        </div>                                            
-                        )
-            })
-        }
-        <br/>
+      </form>
+      {
+        comments?.map((el, i) => {
+          return (
+            <div key={i}>
+              {/* <p>nombre del user</p> */}
+              <p>{el.title}</p>
+              <p>{el.description}</p>
+            </div>
+          )
+        })
+      }
+      <br />
     </div>
-)
+  )
 }
 
 export default Comments;
