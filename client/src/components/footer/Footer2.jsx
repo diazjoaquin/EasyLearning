d0755b0b8be984fad7eafcfa0c1acd837ae550f6
ggolder22 <germanxvzy985@hotmail.com>
2022-12-12 15:50:21 -0300

    footer logo changed

--- client/src/components/footer/Footer2.jsx
+++ client/src/components/footer/Footer2.jsx
@@ -17,8 +17,9 @@ import { FaTwitter, FaYoutube } from 'react-icons/fa';
 import { BiMailSend } from 'react-icons/bi';
 // import Logo from "../../image/easylearning.png"
 import { Button, ButtonGroup } from '@chakra-ui/react'
-import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa"
-import Logo from "../footer/easylearning.png"
+import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
+import Logo from "./easylearning.png";
+
 
 
 
@@ -69,8 +70,8 @@ export default function Footer2() {
                     templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
                     spacing={8}>
                     <Stack spacing={6}>
-                        <Box>
-                            <img src="Logo" alt="logo" />
+                        <Box size="xs">
+                            <img width="270px" src={Logo} alt="logo" />
                         </Box>
                         <Text fontSize={'sm'}>
                             © 2022 easyLearning. All rights reserved
@@ -92,7 +93,7 @@ export default function Footer2() {
 
                             <Button colorScheme='facebook' leftIcon={<FaFacebook />}> <a href="https://es-la.facebook.com/" target="_blank">Facebook</a></Button>
                             <Button colorScheme="linkedin" leftIcon={<FaLinkedin />} > <a href="https://www.linkedin.com/feed/" target="_blank">Linkedin</a></Button>
-                            <Button colorScheme="instagram" leftIcon={<FaInstagram />}> <a href="https://www.instagram.com/" target="_blank">Instagram</a></Button>
+                            <Button  leftIcon={<FaInstagram />}> <a href="https://www.instagram.com/" target="_blank">Instagram</a></Button>
 
 
 
