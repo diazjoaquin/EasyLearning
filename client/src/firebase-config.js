ac3aac43908ec4cfbe253956fbf35b0353873e61
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2022-12-12 22:28:25 -0300

    firebase config added

---
+++ client/src/firebase-config.js
@@ -0,0 +1,12 @@
+export default {
+    
+        apiKey: "AIzaSyD48-m6BpBJXVQanmLp7aBWWFUSHUNedLg",
+        authDomain: "easylearning-fp.firebaseapp.com",
+        databaseURL: "https://easylearning-fp-default-rtdb.firebaseio.com",
+        projectId: "easylearning-fp",
+        storageBucket: "easylearning-fp.appspot.com",
+        messagingSenderId: "858975070973",
+        appId: "1:858975070973:web:1d2a790bf1cd14ff42ac94",
+        measurementId: "G-ZSCERXL0XX",
+
+};
