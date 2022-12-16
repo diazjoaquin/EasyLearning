894798c6ccc0a0663232e6d2f96ca7f19e9b8a36
fermin234 <fermin234@hotmail.com>
2022-12-16 00:26:06 -0300

    add avatar and acordion

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,55 +6,64 @@ import { signOut } from "firebase/auth";
 import { Avatar } from '@chakra-ui/react';
 import { useAuth } from "../context/Auth-context";
 import { auth } from "../../firebase-config";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+} from '@chakra-ui/react'
 
 export default function Navbar() {
 
-    const { user, logout, loading } = useAuth();
-    const handleLogout = async () => {
-        await logout(auth);
-    };
+  const { user, logout, loading } = useAuth();
+  const handleLogout = async () => {
+    await logout(auth);
+  };
 
-    console.log(user);
+  if (loading) { return <h1>Loading ...</h1> }
 
-    if (loading) {return <h1>Loading ...</h1>}
+  return (
+    <div className={style.navcont}>
+      <div className={style.botones}>
+        <img className={style.logo} src={Logo} alt="Logo" />
 
-    return (
-        <div className={style.navcont}>
-            <div className={style.botones}>
-                <img className={style.logo} src={Logo} alt="Logo" />
-                
-                <div className={style.menu}>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/course">Course</Link>
-                    <Link to="/blog">Blog</Link>
-                    <Link to="/contact">Contact</Link>
-                    <div className={style.buttons}>
-                        {user && <Avatar src='https://bit.ly/broken-link' bg='teal.500' size='sm'/>}
-                        {user && <Button colorScheme='teal' variant='solid'
-                        onClick={handleLogout}>
-                                Log Out
-                            </Button>}
+        <div className={style.menu}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/course">Course</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+          <div className={style.buttons}>
+            {!user && <Link to="/login">
+              <Button colorScheme='gray'>
+                Login
+              </Button></Link>}
 
-                        {!user && <Link to="/login">
-                             <Button colorScheme='gray'>
-                                Login
-                            </Button></Link>}
-                        
-                        {!user && <Link to="/signup">
-                            <Button colorScheme='teal' variant='solid'>
-                                Sign Up
-                            </Button>
-                        </Link>}
+            {!user && <Link to="/signup">
+              <Button colorScheme='teal' variant='solid'>
+                Sign Up
+              </Button>
+            </Link>}
 
-
-                        
-                    </div>
-                    
-                    
-                </div>
-
-            </div>
+            {user &&
+              <Menu>
+                <MenuButton >
+                  <Avatar src='https://bit.ly/broken-link' bg='teal.500' size='sm' />
+                </MenuButton>
+                <MenuList>
+                  <MenuGroup title='Profile'>
+                    <Link to='/profile'>
+                      <MenuItem>My Account</MenuItem>
+                    </Link>
+                    <MenuItem onClick={handleLogout}> Log out </MenuItem>
+                  </MenuGroup>
+                </MenuList>
+              </Menu>
+            }
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
