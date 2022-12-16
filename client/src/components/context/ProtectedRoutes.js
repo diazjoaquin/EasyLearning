5c3c6353d15a6defd230ed0bad7d0c49c14c7638
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2022-12-15 23:08:36 -0300

    protected routes added

--- client/src/components/context/ProtectedRoutes.js
+++ client/src/components/context/ProtectedRoutes.js
@@ -1,10 +1,8 @@
 import { useAuth } from "./Auth-context";
-import { Navigate } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 
 export const ProtectedRoutes = ({children}) => {
     const { user, loading } = useAuth();
-
     const history = useHistory();
 
     if (loading) return <h1>Loading ...</h1>;
