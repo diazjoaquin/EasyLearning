e630de9bbdff57ce4e12b16a0d520b5d33ad566c
Fran3003 <franco.rantuchoQgmail.com>
2023-01-10 14:28:30 -0300

    alcualizacion

--- server/src/Routes/deleteComments/deleteComments.js
+++ server/src/Routes/deleteComments/deleteComments.js
@@ -6,9 +6,6 @@ router.delete("/:id/:string", async (req, res) => {
     const id = req.params.id
     const string = req.params.string
     try {
-        
-        console.log(id);
-        console.log(string);
         res.status(204).json(await deleteComments(id, string))
     } catch (error) {
       res.json(error);
